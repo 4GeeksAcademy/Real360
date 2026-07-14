465747b2e1d40c4e9569c48c1d1d050e82c4a7c6
marioramirezlopez98-del <marioramirez.lopez98@gmail.com>
2026-07-14 02:26:39 +0000

    Ajuste correo de confirmación resetpassword

--- src/front/pages/ResetPassword.jsx
+++ src/front/pages/ResetPassword.jsx
@@ -13,6 +13,55 @@ export const ResetPassword = () => {
             navigate('/portal/dashboard'); 
     };
 
+    const handleSubmit = async () => {
+        if(password === "" || newPassword === "" || confirmNewPassword === ""){
+            alert("Error, datos incorrectos")
+            return
+        }
+        if(newPassword != confirmNewPassword){
+            alert("Error, las contraseñas no coinciden")
+            return
+        }
+        if(newPassword.length < 8 || newPassword.length > 20){
+                alert("error, la contraseña debe tener entre 8 y 20 caracteres")
+                return
+            }
+        await UpdatePassword(password, newPassword)
+
+    }
+
+    const UpdatePassword = async (password, newPassword) => {
+
+        const tokenGuardado = localStorage.getItem("token");
+
+        try {
+            const response = await fetch("https://scaling-funicular-g49wp9x6qw57c9p5q-3001.app.github.dev/api/change-password" , {
+                method: "POST",
+                body: JSON.stringify({
+                    "current_password": password,
+                    "new_password": newPassword
+                }),
+                headers: {
+                    'content-type': 'application/json',
+                    'Authorization': 'Bearer ' + tokenGuardado
+                }
+            })
+            const data = await response.json()
+
+            if(response.ok){
+                alert(data.msg)
+
+                navigate('/login')
+            }
+            else{
+                alert(data.msg)
+            }
+        }   
+        catch(error){
+            console.error("Error uploading password: ", error)
+        }
+    }
+
     return (
         <div>
             <div className="p-2 m-2 w-75">
@@ -30,7 +79,7 @@ export const ResetPassword = () => {
                 <input type="password" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock" value={confirmNewPassword} onChange={(event)=>setConfirmNewPassword(event.target.value)}/>
             </div>
             <div className="m-2 p-2">
-                <button type="button" className="btn btn-primary m-2">Enviar</button>
+                <button type="button" className="btn btn-primary m-2" onClick={handleSubmit}>Enviar</button>
                 <button type="button" className="btn btn-danger m-2" onClick={returnToDashboard}>Cancelar</button>
             </div>
         </div>
